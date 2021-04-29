0fdebc15c60865d1c69277d029e918020335097d
Ufuoma Ubor <foskynew@gmail.com>
2021-04-29 18:21:52 +0100

    bug: overwrite existing file when saving to sqlite

--- data/process_data.py
+++ data/process_data.py
@@ -45,7 +45,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///'+database_filename+'.db')
-    df.to_sql(database_filename, engine, index=False)
+    df.to_sql(database_filename, engine, index=False, if_exists="replace")
 
 
 def main():
