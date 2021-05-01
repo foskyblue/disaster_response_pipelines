9fe73ebb61621d22ca01f7aecaee94a35872e380
Ufuoma Ubor <foskynew@gmail.com>
2021-05-01 02:23:51 +0100

    bug: util file to handle leaky pipes

---
+++ models/util.py
@@ -0,0 +1,30 @@
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+class Utility:
+    def __init__(self):
+        pass
+
+    def tokenize(text):
+        tokens = word_tokenize(text)
+        lemmatizer = WordNetLemmatizer()
+
+        clean_tokens = []
+        for tok in tokens:
+            clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+            clean_tokens.append(clean_tok)
+
+        return clean_tokens
+
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
