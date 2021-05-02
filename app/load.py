44261b01bedb06f3e182fd61f4a11335a10b1ee6
Ufuoma Ubor <foskynew@gmail.com>
2021-05-02 14:28:39 +0100

    docs: adding documentation

--- app/load.py
+++ app/load.py
@@ -4,14 +4,19 @@ from sqlalchemy import create_engine
 
 
 def load_data():
+    """
+    :return: Data frame
+    """
     # load data
     engine = create_engine('sqlite:///../data/disaster_response.db')
     df = pd.read_sql_table('disaster_response', engine)
     return df
-    
+
 
 def load_model():
-
+    """
+    :return: Pickled file used for prediction
+    """
     # load model
     model = joblib.load("../models/disaster_response_pickle.pkl")
 
